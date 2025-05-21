9b031708cb372be86aa84fd63953715b9ab8b3b4
SoniyaAvudari <avudarisoniya@gmail.com>
2025-05-21 11:45:54 +0530

    HELLO GIT

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MyWebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Hello"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
